cce0843a243ccc30c9adddde8cfa6cb80d950611
moliri <lmaliakal@u.northwestern.edu>
2017-09-28 16:15:14 -0500

    can create smaller training sets

--- decisiontree.py
+++ decisiontree.py
@@ -1,4 +1,4 @@
-import sys, csv
+import sys, csv, random
 
 def main():
 	# input = inputFileName, trainingSetSize, numberOfTrials, verbose
@@ -7,14 +7,15 @@ def main():
 		print "ERR: Missing parameters! Make sure to include inputFileName, trainingSetSize, numberOfTrials, and verbose."
 	else: 
 		inputFileName = sys.argv[1]
-		trainingSetSize = sys.argv[2]
+		trainingSetSize = int(sys.argv[2])
 		numberOfTrials = sys.argv[3]
 		verbose = sys.argv[4]
 	
 		# 1. read in inputFileName containing examples
-		getExamples(inputFileName)
-
 		# 2. divide exampes into training set (n = trainingSetSize, chosen randomly) and testing set
+		trainingSet, testingSet = getExamples(inputFileName, trainingSetSize)
+
+		
 		# 3. estimate expected prior probability of True or False classifications based on distribution of examples in training set
 		# 4. construct decision tree based on training set
 		# 5. classify examples in testing set using the tree
@@ -22,18 +23,36 @@ def main():
 		# 7. determine proportion of correct classifications in each ^ by comparing classifications to correct answers
 		# 8. repeat 2-7 (a trial) until numberOfTrials == the number of trials run
 		# 9. print results for each trial (review output format)
-	
 
-
-def getExamples(fileName):
+def getExamples(fileName, trainingSetSize):
 	examplesFile = open(fileName, "r")
-	print "got examples yay!"
-	# print examplesFile.readline()
-
 	examplesDict = csv.DictReader(examplesFile, delimiter='\t')
+	trainingSet = []
+	testingSet = []
+	# i = 0
+
+	# examples = [r for r in examplesDict]
+	# while len(trainingSet) < 10:
+	# 	trainingSet.append(random.choice(examples))
+
 	for example in examplesDict:
-		print example['GoodGrades']
-	print examplesDict
+		# i += 1
+		sorter = random.randint(0,1)
+		examplesNeeded = trainingSetSize - len(trainingSet)
+		# examplesLeft = len(list(examplesDict)) - i+1
+
+		# print "need, left: ", examplesNeeded, examplesLeft
+		# if (examplesNeeded == examplesLeft): 
+			# trainingSet.append(example)
+		if (sorter == 0 and len(trainingSet) < trainingSetSize):
+			trainingSet.append(example)
+		else: 
+			testingSet.append(example)
+
+	print "Training set: ", len(trainingSet)
+	print "Testing set: ", len(testingSet)
+
+	return trainingSet, testingSet
 
 
 
---
+++ demo.py
@@ -0,0 +1,14 @@
+
+
+def main():
+	readExamples()
+	#2 
+
+
+def readExamples():
+	print "hi"
+
+
+
+if __name__ == "__main__":
+	main()
